eff827fa884b6e4d0da5fb317f07f508221401ec
jhilzheber <justine@hilzheber.com>
2019-04-29 10:11:05 +0200

    corrections PSR2

--- sql/import.sql
+++ sql/import.sql
@@ -1,7 +1,17 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
+SET
+SQL_MODE
+=
+"NO_AUTO_VALUE_ON_ZERO";
+SET
+AUTOCOMMIT
+=
+0;
+START
+TRANSACTION;
+SET
+time_zone
+=
+"+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -11,22 +21,25 @@ SET time_zone = "+00:00";
 
 
 
-CREATE TABLE `Users` (
-  `id` int(11) NOT NULL,
-  `firstname` varchar(50) NOT NULL,
-  `lastname` varchar(100) NOT NULL,
-  `email` varchar(250) NOT NULL,
-  `pwd` varchar(60) NOT NULL,
-  `status` tinyint(4) NOT NULL,
-  `role` tinyint(4) NOT NULL,
-  `date_inserted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `date_updated` timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
+CREATE TABLE `Users`
+(
+  `id`            int(11) NOT NULL,
+  `firstname`     varchar(50)  NOT NULL,
+  `lastname`      varchar(100) NOT NULL,
+  `email`         varchar(250) NOT NULL,
+  `pwd`           varchar(60)  NOT NULL,
+  `status`        tinyint(4) NOT NULL,
+  `role`          tinyint(4) NOT NULL,
+  `date_inserted` timestamp    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `date_updated`  timestamp NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 
-INSERT INTO `Users` (`id`, `firstname`, `lastname`, `email`, `pwd`, `status`, `role`, `date_inserted`, `date_updated`) VALUES
-(1, 'John', 'Doo', 'john@doo.com', '$2y$10$aIxgdHwT.l/I/hNxFRt5mOjflQPAGtFevoAb/0X1gUMcl8DCGyGtq', 0, 1, '2019-01-01 00:00:00', NULL);
+INSERT INTO `Users` (`id`, `firstname`, `lastname`, `email`, `pwd`, `status`, `role`, `date_inserted`, `date_updated`)
+VALUES
+(1, 'John', 'Doo', 'john@doo.com', '$2y$10$aIxgdHwT.l/I/hNxFRt5mOjflQPAGtFevoAb/0X1gUMcl8DCGyGtq', 0, 1,
+ '2019-01-01 00:00:00', NULL);
 
 
 ALTER TABLE `Users`
@@ -34,7 +47,7 @@ ALTER TABLE `Users`
 
 
 ALTER TABLE `Users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int (11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
